4a4e9ab949c856f3bf8629d4898f73299e3c7517
iam-malan <64845039+iam-malan@users.noreply.github.com>
2022-10-11 18:51:48 +0200

    Update

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
               <li>
               <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' 
                   href="getintouch">
-                    Contact me!
+                    Contact
                   </a>
 
               </li>
