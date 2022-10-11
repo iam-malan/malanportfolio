6d9f735439724e187c0dccad85517c38847c74d7
iam-malan <64845039+iam-malan@users.noreply.github.com>
2022-10-11 17:49:34 +0200

    Initial commit

---
+++ pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div className='text-center p-10 py-10'>
+        <h2 className='text-5xl py-2 text-teal-600 font-medium'>ABOUT MALAN</h2>
+        <h3 className='text-2xl py-2 dark:text-white'>Developer and Software Designer</h3>
+        <p className='text-md py-5 leading-8 text-gray-800 dark:text-white max-w-xl mx-auto '>Freelancer providing services in a range of languages and frameworks. Contact me below for more info!
+        </p>
+      </div>
+    )
+}
+
+
